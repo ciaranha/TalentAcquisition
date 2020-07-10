901c7530726d4f971af91a0f7fb28ca4e263b9b0
jaibles <ciaran@visible.vc>
2020-07-10 15:29:59 +0100

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueDrift from 'vue-drift'
+import VueAnalytics from 'vue-analytics';
+
 import './assets/styles/index.css';
 
 Vue.use(VueDrift, {
   appId: 'gxkmcap389g5',
 })
+
+Vue.use(VueAnalytics, {
+  id: 'UA-172318941-1'
+});
 
 new Vue({
   render: h => h(App),
