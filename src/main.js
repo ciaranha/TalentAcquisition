e2100554feb8a63b02eb4fb981abadeeae078121
jaibles <ciaran@visible.vc>
2020-07-11 12:21:07 +0100

    switch drift

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueDrift from 'vue-drift'
 import VueGtag from "vue-gtag";
 
 import './assets/styles/index.css';
@@ -8,10 +7,6 @@ import './assets/styles/index.css';
 Vue.use(VueGtag, {
   config: { id: 'UA-172318941-1' }
 });
-
-Vue.use(VueDrift, {
-  appId: 'gxkmcap389g5',
-})
 
 new Vue({
   render: h => h(App),
